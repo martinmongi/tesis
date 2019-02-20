0d5f72744325d92035d02a43e9f0219731991e4d
Martín Mongi Badía <martinmongi@gmail.com>
2019-02-20 02:20:34 -0300

    who knows

--- src/bus_stops.py
+++ src/bus_stops.py
@@ -1,16 +1,28 @@
-def ralen(x):
-    return range(len(x))
 
 
 import cplex
 from collections import defaultdict, deque
 
+
+def ralen(x):
+    return range(len(x))
+
+
+def reverse_graph(g):
+    rg = defaultdict(lambda: {})
+    for v in g:
+        for v2 in g[v]:
+            rg[v2][v] = g[v][v2]
+    return rg
+
+
 # INPUT
-with open('mann10.in', 'r') as f:
+with open('easyloop.in', 'r') as f:
     ls = f.readlines()
     N = eval(ls[0])
     # print(N)
     g = eval(ls[1])
+    rg = reverse_graph(g)
     W = eval(ls[2])
     bs = eval(ls[3])
     sp = eval(ls[4])
@@ -21,7 +33,7 @@ def graphs(vnames, vals):
     dsol = {vnames[i]: vals[i] for i in ralen(vals)}
     for bn in ralen(bs):
         res.append(defaultdict(lambda: []))
-        for v1 in range(N * N):
+        for v1 in g:
             for v2 in g[v1]:
                 if dsol["busedge_" + str(bn) + "_" + str(v1) + "_" + str(v2)] > 0.5:
                     res[-1][v1].append(v2)
@@ -34,101 +46,122 @@ problem.objective.set_sense(problem.objective.sense.minimize)
 
 vnames = ["busedge_" + str(i) + "_" + str(v1) + "_" + str(v2)
           for i in ralen(bs) for v1 in g for v2 in g[v1]] + \
+    ["busvertexin_" + str(i) + "_" + str(v1) for i in ralen(bs) for v1 in g] + \
+    ["busvertexout_" + str(i) + "_" + str(v1) for i in ralen(bs) for v1 in g] +\
     ["busstop_" + str(i) + "_" + str(v1) for i in ralen(bs) for v1 in g]
-vtypes = [problem.variables.type.binary for i in range(
-    len(bs)) for v1 in g for v2 in g[v1]] + \
+
+vtypes = [problem.variables.type.integer for i in ralen(bs) for v1 in g for v2 in g[v1]] + \
+    [problem.variables.type.integer for i in ralen(bs) for v1 in g] + \
+    [problem.variables.type.integer for i in ralen(bs) for v1 in g] + \
     [problem.variables.type.binary for i in ralen(bs) for v1 in g]
+
 vobj = [g[v1][v2] for i in ralen(bs) for v1 in g for v2 in g[v1]] + \
+    [0 for i in ralen(bs) for v1 in g] + \
+    [0 for i in ralen(bs) for v1 in g] + \
     [0 for i in ralen(bs) for v1 in g]
+
+vlb = [0 for i in ralen(bs) for v1 in g for v2 in g[v1]] + \
+    [0 for i in ralen(bs) for v1 in g] + \
+    [0 for i in ralen(bs) for v1 in g] + \
+    [0 for i in ralen(bs) for v1 in g]
+
 # print(vnames)
 
 problem.variables.add(obj=vobj,
-                      lb=None,
+                      lb=vlb,
                       ub=None,
                       types=vtypes,
                       names=vnames)
 
 # CONTSTRAINTS
 
+# Degree constraints
 for i in ralen(bs):
-    for v in range(N * N):
+    for v in g:
+        rhs = [0]
+        sense = 'E'
+        constraint = [
+            ["busvertexout_" + str(i) + "_" + str(v)] +
+            ["busedge_" + str(i) + "_" + str(v) + "_" + str(v2)
+             for v2 in g[v]],
+            [1] + [-1 for v2 in g[v]]
+        ]
+        problem.linear_constraints.add(lin_expr=[constraint],
+                                       senses=sense,
+                                       rhs=rhs)
+
+        constraint = [
+            ["busvertexin_" + str(i) + "_" + str(v)] +
+            ["busedge_" + str(i) + "_" + str(v2) + "_" + str(v)
+             for v2 in rg[v]],
+            [1] + [-1 for v2 in rg[v]]
+        ]
+        problem.linear_constraints.add(lin_expr=[constraint],
+                                       senses=sense,
+                                       rhs=rhs)
+
+        rhs = [0]
+        sense = 'L'
+        constraint = [
+            ["busvertexout_" + str(i) + "_" + str(v),
+             "busvertexin_" + str(i) + "_" + str(v),
+             "busstop_" + str(i) + "_" + str(v)],
+            [-1, -1, 1]
+        ]
+        problem.linear_constraints.add(lin_expr=[constraint],
+                                       senses=sense,
+                                       rhs=rhs)
+
         rhs = [0]
         sense = 'G'
-        for v2 in g[v]:
-            constraint = [
-                ["busstop_" + str(i) + "_" + str(v),
-                 "busedge_" + str(i) + "_" + str(v) + "_" + str(v2)],
-                [1, -1]
-            ]
-            problem.linear_constraints.add(lin_expr=[constraint],
-                                           senses=sense,
-                                           rhs=rhs)
+        constraint = [
+            ["busvertexout_" + str(i) + "_" + str(v),
+             "busvertexin_" + str(i) + "_" + str(v),
+             "busstop_" + str(i) + "_" + str(v)],
+            [-1, -1, len(g)]
+        ]
+        problem.linear_constraints.add(lin_expr=[constraint],
+                                       senses=sense,
+                                       rhs=rhs)
 
 # Para cada salida de bondi, el grado de salida del camino del bondi es 1
 for i in ralen(bs):
     rhs = [1]
     sense = 'E'
     constraint = [
-        ["busedge_" + str(i) + "_" + str(bs[i]) + "_" + str(v2)
-         for v2 in g[bs[i]]],
-        [1 for v2 in g[0]]
+        ["busvertexout_" + str(i) + "_" + str(bs[i]),
+         "busvertexin_" + str(i) + "_" + str(bs[i])],
+        [1, -1]
     ]
     problem.linear_constraints.add(lin_expr=[constraint],
                                    senses=sense,
                                    rhs=rhs,
                                    names=['grado_salida_' + str(bs[i])])
 
-# Para cada salida de bondi, el grado de entrada del camino del bondi es 0
-for i in ralen(bs):
-    rhs = [0]
-    sense = 'E'
-    constraint = [
-        ["busedge_" + str(i) + "_" + str(v1) + "_" + str(v2)
-         for v1 in g for v2 in g[v1] if v2 == bs[i]],
-        [1 for v1 in g for v2 in g[v1] if v2 == bs[i]]
-    ]
-    problem.linear_constraints.add(lin_expr=[constraint],
-                                   senses=sense,
-                                   rhs=rhs)
-
 # Grado de entrada de la escuela = 1, para todos los bondis
 for i in ralen(bs):
-    rhs = [1]
+    rhs = [-1]
     sense = 'E'
     constraint = [
-        ["busedge_" + str(i) + "_" + str(v1) + "_" + str(v2)
-         for v1 in g for v2 in g[v1] if v2 == sp],
-        [1 for v1 in g for v2 in g[v1] if v2 == sp]
+        ["busvertexout_" + str(i) + "_" + str(sp),
+         "busvertexin_" + str(i) + "_" + str(sp)],
+        [1, -1]
     ]
     problem.linear_constraints.add(lin_expr=[constraint],
                                    senses=sense,
                                    rhs=rhs)
 
-# Grado de salida de la escuela = 0, para todos los bondis
-for i in ralen(bs):
-    rhs = [0]
-    sense = 'E'
-    constraint = [
-        ["busedge_" + str(i) + "_" + str(sp) + "_" + str(v2)
-         for v2 in g[sp]],
-        [1 for v2 in g[sp]]
-    ]
-    problem.linear_constraints.add(lin_expr=[constraint],
-                                   senses=sense,
-                                   rhs=rhs)
 # Grado de entrada = Grado de salida
 for i in ralen(bs):
-    for v in range(N * N - 1):
+    for v in g:
         if v == sp or v == bs[i]:
             continue
         rhs = [0]
         sense = 'E'
         constraint = [
-            ["busedge_" + str(i) + "_" + str(v) + "_" + str(v2) for v2 in g[v]] +
-            ["busedge_" + str(i) + "_" + str(v2) + "_" + str(v3)
-             for v2 in g for v3 in g[v2] if v3 == v],
-            [1 for v2 in g[v]] +
-            [-1 for v2 in g for v3 in g[v2] if v3 == v],
+            ["busvertexout_" + str(i) + "_" + str(v),
+             "busvertexin_" + str(i) + "_" + str(v)],
+            [1, -1]
         ]
         # print(v,constraint)
         problem.linear_constraints.add(lin_expr=[constraint],
@@ -138,11 +171,10 @@ for i in ralen(bs):
 # Un bondi pasa por alguno de
 for v in W:
     rhs = [1]
-    sense = 'G'
+    sense = 'E'
     constraint = [
-        ["busedge_" + str(i) + "_" + str(v) + "_" + str(v2)
-         for v2 in g[v] for i in ralen(bs)],
-        [1 for v2 in g[v] for i in ralen(bs)]
+        ["busstop_" + str(i) + "_" + str(v)],
+        [1]
     ]
     # print(v,constraint)
     problem.linear_constraints.add(lin_expr=[constraint],
@@ -160,7 +192,7 @@ class NoSeparateSubToursLazyConstraintCallback(cplex.callbacks.LazyConstraintCal
         for bn in ralen(bs):
             gi = gs[bn]
             # print(gi)
-            seen = [False for i in range(N * N)]
+            seen = [False for i in g]
             q = deque([bs[bn]])
             while len(q) > 0:
                 v = q.popleft()
@@ -170,8 +202,9 @@ class NoSeparateSubToursLazyConstraintCallback(cplex.callbacks.LazyConstraintCal
                     # print(v,v2)
                     if not seen[v2]:
                         q.append(v2)
-            print(bn, [i for i in gi.keys() if seen[i]],
-                  [i for i in gi.keys() if not seen[i]])
+
+            # print(bn, [i for i in gi.keys() if seen[i]],
+            #         [i for i in gi.keys() if not seen[i]])
 
             cycles = [i for i in gi.keys() if not seen[i]]
             if len(cycles) > 0:
@@ -181,14 +214,14 @@ class NoSeparateSubToursLazyConstraintCallback(cplex.callbacks.LazyConstraintCal
                     constraint = [
                         ["busstop_" + str(i) + "_" + str(v) for v in cycles] +
                         ["busedge_" + str(i) + "_" + str(v) + "_" + str(v2)
-                        for v in cycles for v2 in g[v] if v2 not in cycles],
+                         for v in cycles for v2 in g[v] if v2 not in cycles],
                         [1 for v in cycles] +
                         [-1 for v in cycles for v2 in g[v] if v2 not in cycles]
                     ]
                     # print(constraint,sense,rhs)
                     self.add(constraint=constraint,
-                            sense=sense,
-                            rhs=rhs)
+                             sense=sense,
+                             rhs=rhs)
 
 
 problem.register_callback(NoSeparateSubToursLazyConstraintCallback)
